29546ba405c68a014729d29f6c6e9c1a5de75d8e
Lenz Lösche <lenzloesche@gmail.com>
2023-03-07 15:00:39 +0100

    fetch data and display 1 image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import useSWR from "swr";
+import Image from "next/image";
+
+const url = "https://example-apis.vercel.app/api/art";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function HomePage() {
+  const { data } = useSWR(url, fetcher);
+  console.log(data);
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      <Image
+        alt="image"
+        height={data[0].dimensions.height}
+        width={data[0].dimensions.width}
+        src={data[0].imageSource}
+      />
     </div>
   );
 }
