e519d5863145b3f4eaf1ca57cc58042c3815149f
Louis van der Weck <louis.vanderweck@gmail.com>
2023-03-08 16:58:40 +0100

    new folder art-pieces

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import ArtPieces from "./../components/ArtPieces";
 import ArtPiecePreview from "../components/ArtPiecePreview";
 import Spotlight from "../components/Spotlight";
 
-export default function HomePage() {
+export default function SpotlightPage({ data }) {
   if (data === undefined) {
     return <h1>Loading...</h1>;
   }
