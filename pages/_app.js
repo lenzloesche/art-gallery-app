20ba5015bed61fa51a2aed53206eb28377412701
Louis van der Weck <louis.vanderweck@gmail.com>
2023-03-08 16:24:01 +0100

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const url = "https://example-apis.vercel.app/api/art";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data } = useSWR(url, fetcher);
+  console.log(data);
   return (
     <>
       <GlobalStyle />
